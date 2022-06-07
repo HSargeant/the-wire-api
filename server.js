822361247292379834c160ae8bc3e102fef18886
hsargeant <hssoftwareengineering@gmail.com>
2022-06-07 19:59:42 -0400

    add database conenction

--- server.js
+++ server.js
@@ -1,3 +1,158 @@
+// const express = require("express")
+// const app = express()
+// const PORT = 7000
+// const bodyParser= require('body-parser')
+// const MongoClient = require("mongodb").MongoClient
+// const cors = require("cors")
+// require('dotenv').config()
+
+// // const connectionString = process.env.DBCONNECT
+
+// app.use(cors())
+// app.use(bodyParser.urlencoded({ extended: true }))
+
+
+
+// app.listen(process.env.PORT || PORT,()=>{
+//     console.log(`The server is running on port ${PORT}`)
+// })
+
+
+
+//     app.get("/", (req,res)=>{
+//         res.sendFile(__dirname + "/index.html")
+//     })
+//     app.get("/add.html", (req,res)=>{
+//         res.sendFile(__dirname + "/add.html")
+//     })
+//     app.get("/main.js", (req,res)=>{
+//         res.sendFile(__dirname + "/main.js")
+//     })
+//     app.get("/404.html", (req,res)=>{
+//         res.sendFile(__dirname + "/404.html")
+//     })
+    
+//     app.get("/api/characters/:id",async (req,res)=>{
+//         console.log(req.params.id)
+//         const id = req.params.id
+//         if(characters[id]){
+//             res.json(characters[id])
+//         }else  res.sendFile(__dirname + "/404.html")
+    
+      
+//     })
+    
+//     app.get("/api/characters", async (req,res)=>{
+//         res.json(characters)
+//     })
+    
+//     app.get("/api/seasons", async (req,res)=>{
+
+//         res.json(seasons)
+//     })
+    
+//     app.get("/api/deaths", async (req,res)=>{
+//         res.json(deaths)
+//     })
+//     app.get("/api", async (req,res)=>{
+
+//      res.json(characters)
+
+       
+//     })
+
+// //data
+
+// const characters = [
+//     {
+//         "char_id": "0",
+//         "name": "Jimmy Mcnulty",
+//         "nickname": ["Bushy Top"],
+//         "occupation": ["Homicide Detective"],
+//         "seasons": [1, 2, 3, 4, 5],
+//         "DOB": "1971",
+//         "first-seen": "The Target",
+//         "last-seen": "-30-",
+//         "status": "Alive",
+//         "actor": "Dominic West",
+//         "image": "https://static.wikia.nocookie.net/thewire/images/9/92/McNulty.jpg",
+//         "category": "The Law"
+
+//     },
+//     {
+//         "char_id": "1",
+//         "name": "Avon Barksdale",
+//         "nickname": [],
+//         "occupation": ["Head of the Barksdale Organization"],
+//         "seasons": [1, 2, 3, 5],
+//         "DOB": "August 15, 1970",
+//         "status": "Alive",
+//         "actor": "Wood Harris",
+//         "image": "https://static.wikia.nocookie.net/thewire/images/a/aa/Avon.jpg",
+//         "category": "The Street"
+
+//     },
+
+//     {
+//         "char_id": "2",
+//         "name": "Omar Little",
+//         "nickname": [],
+//         "occupation": ["Stickup Man"],
+//         "first-seen": "The Buys",
+//         "last-seen": "Clarifications",
+//         "seasons": [1, 2, 3, 4, 5],
+//         "DOB": "1974",
+//         "status": "Dead",
+//         "actor": "Michael K. Williams",
+//         "image": "https://static.wikia.nocookie.net/thewire/images/4/46/Omar.jpg",
+//         "category": "The Street"
+
+//     },
+//     {
+//         "char_id": "3",
+//         "name": "William Moreland",
+//         "nickname": ["Bunk"],
+//         "occupation": ["Homicide Detective"],
+//         "first-seen": "The Target",
+//         "last-seen": "-30-",
+//         "seasons": [1, 2, 3, 4, 5],
+//         "DOB": "unknown",
+//         "status": "Alive",
+//         "actor": "Wendell Pierce",
+//         "image": "https://static.wikia.nocookie.net/thewire/images/1/19/Bunk.jpg",
+//         "category": "The Law"
+
+//     },
+//     {
+//         "char_id": "4",
+//         "name": "Shakima Greggs",
+//         "nickname": ["Kima"],
+//         "occupation": ["Homicide Detective"],
+//         "first-seen": "The Target",
+//         "last-seen": "-30-",
+//         "seasons": [1, 2, 3, 4, 5],
+//         "status": "Alive",
+//         "actor": "Sonja Sohn",
+//         "image": "https://static.wikia.nocookie.net/thewire/images/b/b9/The_Wire_Greggs.jpg",
+//         "category": "The Law"
+
+//     }
+
+// ]
+
+// const seasons = []
+
+// const deaths = [
+//     {
+//         "name": "Omar Little",
+//         "cause": "Shot in the head",
+//         "responsible": "Kenard",
+//         "episode": "",
+//         "season": ""
+//     }
+// ]
+
+
 const express = require("express")
 const app = express()
 const PORT = 7000
@@ -6,7 +161,7 @@ const MongoClient = require("mongodb").MongoClient
 const cors = require("cors")
 require('dotenv').config()
 
-// const connectionString = process.env.DBCONNECT
+const connectionString = process.env.DBCONNECT
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -18,6 +173,11 @@ app.listen(process.env.PORT || PORT,()=>{
 })
 
 
+MongoClient.connect(connectionString)
+.then(client=>{
+    console.log('connected to Db')
+    const db = client.db('the-wire-api')
+    // const characters = db.collection('characters')
 
     app.get("/", (req,res)=>{
         res.sendFile(__dirname + "/index.html")
@@ -35,37 +195,42 @@ app.listen(process.env.PORT || PORT,()=>{
     app.get("/api/characters/:id",async (req,res)=>{
         console.log(req.params.id)
         const id = req.params.id
-        if(characters[id]){
-            res.json(characters[id])
+        let chars = await db.collection('characters').find().toArray()
+        if(chars[id]){
+            res.json(chars[id])
         }else  res.sendFile(__dirname + "/404.html")
     
       
     })
     
     app.get("/api/characters", async (req,res)=>{
-        res.json(characters)
+        let chars =  await db.collection('characters').find().toArray()
+        // console.log(chars)
+        res.json(chars)
     })
     
     app.get("/api/seasons", async (req,res)=>{
-
-        res.json(seasons)
+        let seasons1 = await db.collection('seasons').find().toArray()
+        res.json(seasons1)
     })
     
     app.get("/api/deaths", async (req,res)=>{
+        let deaths = await db.collection('deaths').find().toArray()
         res.json(deaths)
     })
     app.get("/api", async (req,res)=>{
-
-     res.json(characters)
+        let chars = await db.collection('characters').find().toArray()
+     res.json(chars)
 
        
     })
+})
 
 //data
 
 const characters = [
     {
-        "char_id": "0",
+        "char_id": "1",
         "name": "Jimmy Mcnulty",
         "nickname": ["Bushy Top"],
         "occupation": ["Homicide Detective"],
@@ -80,7 +245,7 @@ const characters = [
 
     },
     {
-        "char_id": "1",
+        "char_id": "2",
         "name": "Avon Barksdale",
         "nickname": [],
         "occupation": ["Head of the Barksdale Organization"],
@@ -94,7 +259,7 @@ const characters = [
     },
 
     {
-        "char_id": "2",
+        "char_id": "3",
         "name": "Omar Little",
         "nickname": [],
         "occupation": ["Stickup Man"],
@@ -109,7 +274,7 @@ const characters = [
 
     },
     {
-        "char_id": "3",
+        "char_id": "4",
         "name": "William Moreland",
         "nickname": ["Bunk"],
         "occupation": ["Homicide Detective"],
@@ -150,4 +315,5 @@ const deaths = [
         "episode": "",
         "season": ""
     }
+
 ]
