bac04b405b62d88733a1da9dcd4d208187e3633f
hsargeant <hssoftwareengineering@gmail.com>
2022-06-08 00:38:05 -0400

    update server

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ require('dotenv').config()
 
 let db 
 let dbName = 'the-wire-api'
-const connectionString = process.env.DBCONNECT
+const connectionString = process.env.MONGO_URI
     
 MongoClient.connect(connectionString,{ useUnifiedTopology: true })
     .then(client => {
