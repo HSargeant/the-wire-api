0633628acee5526e22be83cd2c32c26369594b71
hsargeant <hssoftwareengineering@gmail.com>
2022-06-20 18:33:29 -0400

    count

---
+++ README.md
@@ -0,0 +1,87 @@
+# The Wire API
+
+## Documentation
+
+The API for the seminal TV series. This documentation will help
+with all you need to begin making your request.
+
+## Base URL
+https://the-wire-api.herokuapp.com/api
+
+## Rate Limit
+This API is free, there is a rate limit of ______ requests a day. If reach the limit, you will regain access after 24 hours.
+
+## Character Endpoints
+
+#### Character Attributes
+
+| Attribute   | Type          | Description  									  |
+| ----------- | ------------- | ------------------------------------------------- |
+| char_id     | string  	  | Unique Id per character							  |
+| name        | string        | A character's full name 						  |
+| occupation  | array         | List of known occupation/s						  |
+| image       | string        | Character's image								  |
+| status      | string        | Are the still in the game or dead    			  |
+| aka		  | string        | A nickname or alias the character is referred as  |
+| seasons	  | array         | List of seasons that the character appeared in    |
+
+#### Get info about all characters
+Endpoint to retrieve information from all characters.
+```
+/api/characters
+```
+
+#### Get single character
+```
+by id#: /api/characters/1
+by name: /api/characters/name
+		*** can use first name, last name, or nickname/alias ***
+		***	examples: '/api/characters/bunk' , '/api/characters/the greek'
+``` 
+
+## Quote Endpoints
+
+#### Quote Attributes
+
+| Attribute   | Type          | Description  					  |
+| ----------- | ------------- | --------------------------------  |
+| quote       | string        | The quote						  |
+| name	      | string        | The character that said the quote |
+
+#### Get all quotes
+```
+/api/quotes
+```
+
+#### Get a random quote
+```
+/api/quotes/random
+```
+
+#### Get quote by author
+```
+/api/quote/Bunk, /api/quote/Mcnulty etc... 
+```
+This endpoint allows you to get all quotes from a specific character.
+
+## Death Endpoints
+
+#### Death Attributes
+
+| Attribute         | Type          | Description  												 |
+| ----------------- | ------------- | -----------------------------------------------------------|
+| name              | string        | The name of the deceased character 						 |
+| cause             | string        | How the character died		  							 |
+| responsible       | string        | Who is directly responsible for their death				 |
+| season            | integer       | The season the death occured in   						 |
+| episode           | integer       | The episode of the season									 |
+
+#### Get all death information
+```
+/api/deaths
+```
+
+#### Get random death information about an individual
+```
+/api/deaths/random-death
+```
--- server.js
+++ server.js
@@ -9,10 +9,11 @@ const MongoClient = require("mongodb").MongoClient
 const cors = require("cors")
 require('dotenv').config()
 
+
 let db 
 let dbName = 'the-wire-api'
 const connectionString = process.env.MONGO_URI
-    
+
 MongoClient.connect(connectionString,{ useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
@@ -38,22 +39,21 @@ MongoClient.connect(connectionString,{ useUnifiedTopology: true })
 
     app.get("/", (req,res)=>{
         res.sendFile(__dirname + "/index.html")
-        
+
     })
 
-    // app.get("/main.js", (req,res)=>{
-    //     res.sendFile(__dirname + "/main.js")
-    // })
-    // app.get("/404.html", (req,res)=>{
-    //     res.sendFile("/404.html")
-    // })
-    app.get("/api",cache('2 minutes'), (req,res)=>{
-        res.json({"characters":"/api/characters","quotes":"/api/quotes","deaths":"/api/deaths"})
-        
+    app.get("/api", async (req,res)=>{
+        res.json({"characters":"/api/characters","quotes":"/api/quotes","deaths":"/api/deaths"})   
+        let visitorCount = await db.collection('count').find().toArray()
+
+        await db.collection("count").updateOne({'count': visitorCount[0].count},{$set: {"count":visitorCount[0].count+1}},function(err, res) {
+            if (err) throw err;
+            console.log("1 document updated")
+        })
+
     })
     
     app.get("/api/characters/:id",cache('2 minutes'),async (req,res)=>{
-        // console.log(req.params.id)
         const id = req.params.id
         let chars = await db.collection('characters').find().toArray()
 
@@ -61,7 +61,15 @@ MongoClient.connect(connectionString,{ useUnifiedTopology: true })
             res.json(chars.find(elem=>elem.char_id == id))
         }else  if(chars.find(elem=>elem.name.toLowerCase().includes(id.toLowerCase()))){
             res.json(chars.find(elem=>elem.name.toLowerCase().includes(id.toLowerCase())))
-        }else res.send("Incorrect syntax. take a look at the documentation. Try endpoint: /api/characters/1")
+        }else res.send("Incorrect syntax. take a look at the documentation. Try endpoint: /api/characters/1 or /api/characters/stringer")
+
+
+        let visitorCount = await db.collection('count').find().toArray()
+
+        await db.collection("count").updateOne({'count': visitorCount[0].count},{$set: {"count":visitorCount[0].count+1}},function(err, res) {
+            if (err) throw err;
+            console.log("1 document updated")
+        })
 
     
       
@@ -70,43 +78,128 @@ MongoClient.connect(connectionString,{ useUnifiedTopology: true })
     app.get("/api/characters",cache('2 minutes'), async (req,res)=>{
         let chars1 =  await db.collection('characters').find().toArray()
         res.json(chars1)
+
+        let visitorCount = await db.collection('count').find().toArray()
+
+        await db.collection("count").updateOne({'count': visitorCount[0].count},{$set: {"count":visitorCount[0].count+1}},function(err, res) {
+            if (err) throw err;
+            console.log("1 document updated")
+        })
     })
     
-    app.get("/api/quotes",cache('2 minutes'), async (req,res)=>{
-        
-        let seasons1 = await db.collection('quotes').find().toArray()
-        // console.log(seasons1.filter(x=>x.name.toLowerCase().includes("str")))
-        res.json(seasons1)
+ 
+    //Quotes
+    app.get("/api/quotes/random", async (req,res)=>{
+        let quotes = await db.collection('quotes').find().toArray()
+        res.json(quotes[Math.floor(Math.random()*quotes.length)])
     })
     app.get("/api/quotes/:name",cache('2 minutes'),async (req,res)=>{
-        let name = req.params.name
+        let name = req.params.name.toLocaleLowerCase()
         let quotes = await db.collection('quotes').find().toArray()
-        if(quotes.filter(x=>x.name.toLowerCase().includes(name.toLowerCase())).length>0){
-            res.json(quotes.filter(x=>x.name.toLowerCase().includes(name.toLowerCase())))
-        } else if(quotes.filter(elem=>elem.name.toLowerCase() == name.toLowerCase())){
-                res.json(quotes.filter(elem=>elem.name.toLowerCase() == name.toLowerCase()))
-    
+        if(quotes.filter(x=>x.name.toLowerCase().includes(name)).length>0){
+            res.json(quotes.filter(x=>x.name.toLowerCase().includes(name)))
+        } else if(quotes.filter(elem=>elem.name.toLowerCase() == name).length>0){
+                res.json(quotes.filter(elem=>elem.name.toLowerCase() == name))
             }else res.send("Incorrect syntax. take a look at the documentation. Try endpoint: /api/quotes/Bunk")
-        // console.log(req.params.name,quotes.filter(elem=>elem.name.toLowerCase() == name.toLowerCase()))
 
-        // if(quotes.filter(elem=>elem.name.toLowerCase() == name.toLowerCase())){
-        //     res.json(quotes.filter(elem=>elem.name.toLowerCase() == name.toLowerCase()))
 
-        // }else  if(quotes.filter(elem=>elem.name.toLowerCase().includes(name.toLowerCase().length>0))){
-        //     res.json(quotes.filter(x=>x.name.toLowerCase().includes("str")))
-        // }else res.send("Incorrect syntax. take a look at the documentation. Try endpoint: /api/characters/1")
+
+            let visitorCount = await db.collection('count').find().toArray()
+
+            await db.collection("count").updateOne({'count': visitorCount[0].count},{$set: {"count":visitorCount[0].count+1}},function(err, res) {
+                if (err) throw err;
+                console.log("1 document updated")
+            })
+    })
+
+    app.get("/api/quotes",cache('2 minutes'), async (req,res)=>{
+        let seasons1 = await db.collection('quotes').find().toArray()
+        res.json(seasons1)
+
+        let visitorCount = await db.collection('count').find().toArray()
+
+        await db.collection("count").updateOne({'count': visitorCount[0].count},{$set: {"count":visitorCount[0].count+1}},function(err, res) {
+            if (err) throw err;
+            console.log("1 document updated")
+        })
     })
     
+
+    //deaths
     app.get("/api/deaths", cache('2 minutes'),async (req,res)=>{
         let deaths = await db.collection('deaths').find().toArray()
         res.json(deaths)
+
+
+        let visitorCount = await db.collection('count').find().toArray()
+
+        await db.collection("count").updateOne({'count': visitorCount[0].count},{$set: {"count":visitorCount[0].count+1}},function(err, res) {
+            if (err) throw err;
+            console.log("1 document updated")
+        })
     })
 
-    //dev testing
-    // app.get("/api", async (req,res)=>{
-    //     let chars = await db.collection('characters').find().toArray()
-    //  res.json(chars)
-    // })
+    app.get("/api/deaths/:whodidit", cache('2 minutes'),async (req,res)=>{
+        let deaths = await db.collection('deaths').find().toArray()
+        let whodidit = req.params.whodidit.toLocaleLowerCase()
+        if(deaths.filter(x=>x.responsible.toLowerCase().includes(whodidit)).length>0){
+            console.log(whodidit)
+            res.json(deaths.filter(x=>x.responsible.toLowerCase().includes(whodidit)))
+        } else if(deaths.filter(elem=>elem.responsible.toLowerCase() == whodidit).length>0){
+                res.json(quotes.filter(elem=>elem.responsible.toLowerCase() == whodidit))
+            }else res.send("Incorrect syntax. take a look at the documentation. Try endpoint: /api/quotes/Bunk")
+
+
+            let visitorCount = await db.collection('count').find().toArray()
+
+            await db.collection("count").updateOne({'count': visitorCount[0].count},{$set: {"count":visitorCount[0].count+1}},function(err, res) {
+                if (err) throw err;
+                console.log("1 document updated")
+            })
+    })
+    
+    //character by category
+    app.get("/api/category/:category", cache('2 minutes'),async (req,res)=>{
+        let category =req.params.category.toLowerCase()
+        let chars = await db.collection('characters').find().toArray()
+        switch(category){
+            case "the street":
+                res.json(chars.filter(elem=>elem.category =="The Street"))
+                break
+            case "the port":
+                res.json(chars.filter(elem=>elem.category =="The Port"))
+                break
+            case "city hall":
+                res.json(chars.filter(elem=>elem.category =="City Hall"))
+                break
+
+            default:
+            res.status(404).json({"characters":"/api/characters","quotes":"/api/quotes","deaths":"/api/deaths"})
+
+
+        }
+
+
+        let visitorCount = await db.collection('count').find().toArray()
+
+        await db.collection("count").updateOne({'count': visitorCount[0].count},{$set: {"count":visitorCount[0].count+1}},function(err, res) {
+            if (err) throw err;
+            console.log("1 document updated")
+        })
+        // if(category =="the street"){
+        //     res.json(chars.filter(elem=>elem.category =="The Street"))
+        // }else if(category =="the port"){
+        //     res.json(chars.filter(elem=>elem.category =="The Port"))
+        // }
+        // if(category =="city hall"){
+        //     res.json(chars.filter(elem=>elem.category =="City Hall"))
+        // }
+    })
+
+    app.get('*', function(req, res){
+        res.status(404).json({"characters":"/api/characters","quotes":"/api/quotes","deaths":"/api/deaths"});
+      });
+
     app.listen(process.env.PORT || PORT,()=>{
         console.log(`The server is running on port ${PORT}`)
     })
